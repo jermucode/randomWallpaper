039eaca310d8e690c2e09eda34a1992e870506a9
Jeremy Heyno <jeremy.heyno@gmail.com>
2020-07-28 17:57:40 +0300

    Remove unnecessary echo tests

--- randomWall.sh
+++ randomWall.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 my_array=(/home/jermu/Downloads/wallpapers/*)
-
-#echo ${#my_array[@]}
-#echo ${my_array[1]}
-#echo $(( $RANDOM % 26))
-#echo $(( $RANDOM % ${#my_array[@]}))
-#wal -i ${my_array[$number]}
-
 wal -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]}
 
